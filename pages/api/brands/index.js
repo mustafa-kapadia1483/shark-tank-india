973bb5d9f1ef958da0be216fe9fef58dd586e26a
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-13 17:51:24 +0530

    api setup to get all brand names

--- pages/api/brands/index.js
+++ pages/api/brands/index.js
@@ -1,5 +1,19 @@
-const handler = () => {
-  return;
+import googleSheetsAuth from "../../../helpers/googleSheetsAuth";
+
+const handler = async (req, res) => {
+  const sheets = await googleSheetsAuth();
+
+  // Brand id from the url
+  const range = "brands!A2:C118";
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range,
+  });
+  const data = response.data.values;
+
+  if (data) {
+    res.status(200).json({ brands: data });
+  }
 };
 
 export default handler;
