d71e33a201e82619dfa6735a739b7e01a939aaf1
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-22 13:47:22 +0530

    setting theme to dark

--- theme.js
+++ theme.js
@@ -1,6 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
   fonts: {
     heading: "Poppins, sans-serif",
     body: "Poppins, sans-serif",
