276d67dabda478a3d4b13d4d37f28db175d1677c
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-23 22:42:18 +0530

    Format data to json

--- pages/api/brands/[brandId].js
+++ pages/api/brands/[brandId].js
@@ -11,8 +11,13 @@ const handler = async (req, res) => {
     `brands!A${brandRow}:C${brandRow}`
   );
   const data = response.data.values[0];
+  const jsonData = {
+    id: data[0],
+    name: data[1],
+    idea: data[2],
+  };
 
-  res.status(200).json({ brand: data });
+  res.status(200).json({ brand: jsonData });
 };
 
 export default handler;
