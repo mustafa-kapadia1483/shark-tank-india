53c0bff2fb15252489875434d7a3d407a8533aa0
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-23 14:27:47 +0530

    added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,15 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="description"
+            content="Shark Tank India Stats Page, view brands that came on shark tank, the deals the got. Everything about shark tank india in one place"
+          />
+          <meta name="robots" content="index,follow" />
+          <meta name="googlebot" content="index,follow" />
+          <meta name="theme-color" content="#4285f4" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
