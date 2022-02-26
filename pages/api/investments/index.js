c47601082ceb3b40bdacbc09daf8b84f0e1f25d8
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-02-27 00:31:47 +0530

    changing sheet range

--- pages/api/investments/index.js
+++ pages/api/investments/index.js
@@ -4,7 +4,7 @@ import queryGoogleSheet from "../../../helpers/queryGoogleSheet";
 
 const handler = async (req, res) => {
   const sheets = await googleSheetsAuth();
-  const response = await queryGoogleSheet(sheets, "investments_cleaned!A1:L95");
+  const response = await queryGoogleSheet(sheets, "investments!A1:P122");
   const data = response.data.values;
   const jsonData = getJsonArrayFromData(data);
   if (jsonData) {
