8f85a60e7b6f30edc5de0345822e8c677c3c9778
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-03-06 00:18:13 +0530

    auth

--- helpers/googleSheetsAuth.js
+++ helpers/googleSheetsAuth.js
@@ -1,6 +1,27 @@
+import os from "os";
+import { promises as fsp } from "fs";
+import path from "path";
 import { google } from "googleapis";
 
+let isSecretsJson = false;
+
+const createSecretsJson = () => {
+  const baseDir = await fsp.mkdtemp(
+    (await fsp.realpath(os.tmpdir())) + path.sep
+  );
+  const fileName = path.join(baseDir, "credentials.json");
+  const buffer = Buffer.from(process.env.GOOGLE_CREDENTIALS, "base64");
+  await fsp.writeFile(fileName, buffer);
+
+  process.env["GOOGLE_APPLICATION_CREDENTIALS"] = fileName;
+
+  return true;
+};
+
 const googleSheetsAuth = async () => {
+  if (!isSecretsJson) {
+    isSecretsJson = createSecretsJson();
+  }
   const googleAuth = await google.auth.getClient({
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
