e3f3d3e95d0562be8277c04359493c1c981aa330
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-05-21 13:39:58 +0530

    sitemap and robots.txt generation at buildtime

---
+++ sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://sharktankindia.mustafak.dev",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
