b4c9894b45e2339824d1315771ea40cc31ad5a00
Mustafa Kapadia <mustafakapadia76@gmail.com>
2023-05-06 12:07:52 +0530

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,13 @@ import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "G-TPV9B84QC4" });
+  }, []);
   return (
     <ContextProvider>
       <ChakraProvider theme={theme}>
